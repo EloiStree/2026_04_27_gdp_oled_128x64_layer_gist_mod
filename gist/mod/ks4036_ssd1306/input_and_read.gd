6e2c4db99db0c487d896f47cf00fea64a2f18174
Éloi Strée <eloistree@gmail.com>
2026-05-18 07:45:57 +0200

    Update input_and_read.gd

--- gist/mod/ks4036_ssd1306/input_and_read.gd
+++ gist/mod/ks4036_ssd1306/input_and_read.gd
@@ -1,5 +1,43 @@
 extends Node3D
 
+# TO COMMUNICATE WITH THE CAR through NodePath
+@onread var car_input = "$%KS4036Input"
+@onread var car_read = "$%KS4036Read"
+
+
+"""
+# TO COMMUNICATE WITH THE CAR throught static func
+# READ INFO
+SensorKs4036ReadSensors.get_instance()
+func is_left_line_on() -> bool:
+func is_right_line_on() -> bool:
+func get_front_distance_in_meter() -> float:
+func get_light_intensity_left() -> float:
+func get_light_intensity_right() -> float:
+func get_color_line_left() -> Color:
+func get_color_line_right() -> Color:
+func is_power_on() -> bool:
+func get_global_position() -> Vector3:
+func get_global_forward_direction() -> Vector3:
+func get_global_quaternion() -> Quaternion:
+func get_global_euler_rotation() -> Vector3:
+
+# SEND INPUT INFO
+SensorKs4036InputRelay.get_instance()
+func set_input_with_array(input_joystick: Vector2) -> void:
+func set_left_wheel_percent_11(percent: float) -> void:
+func set_right_wheel_percent_11(percent: float) -> void:
+func set_wheels_percent_11(left_percent: float, right_percent: float) -> void:
+func set_wheels_motors(top_left: bool, top_right: bool, bottom_left: bool, bottom_right: bool) -> void:
+func set_top_left_motor_button(set_motor_on: bool) -> void:
+func set_top_right_motor_button(set_motor_on: bool) -> void:    
+func set_down_left_motor_button(set_motor_on: bool) -> void:
+func set_down_right_motor_button(set_motor_on: bool) -> void:
+func set_color_led_front_left(color: Color) -> void:    
+func set_color_led_front_right(color: Color) -> void:
+func set_color_led_under_car(color_front_left: Color, color_front_right: Color, color_back_left: Color, color_back_right: Color) -> void:
+"""
+
 func append_layer(array: Array[bool]) -> void:
     # Clear or initialize pattern
     for i in range(8192):
